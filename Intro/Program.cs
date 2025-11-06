c859a37255164eb04e1fc0d37e049a8ba2a1e235
Voxic <m.hille@voxicgames.com>
2025-11-06 12:07:54 +0100

    Very basic monster and geralt battle simulation with 1 attack being executed

--- Intro/Program.cs
+++ Intro/Program.cs
@@ -26,3 +26,15 @@ Console.WriteLine("This is the story of Geralt. He belongs to the guild of " + g
 int monsterHealth = 80;
 int monsterStrength = 2;
 int monsterAgility = 3;
+
+// Simple battle simulation
+// Geralt tries to slay the monster
+Console.WriteLine("Monster attacks Geralt");
+geraltsHealth -= monsterStrength;
+
+Console.WriteLine("Geralt attacks monster");
+monsterHealth -= geraltsStrength;
+
+Console.WriteLine("Geralt's health: " + geraltsHealth);
+Console.WriteLine("Monster's health: " + monsterHealth);
+
