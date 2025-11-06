13cec1c23d16e2c2243fca56e189f156adf42fa7
Voxic <m.hille@voxicgames.com>
2025-11-06 09:30:51 +0100

    Listed some primitive data types

--- Intro/Program.cs
+++ Intro/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿// string - text
+// bool - true or false
+// int - whole number 
+// float - floating point number
+// double - just bigger floating point number
+// char - single character
+// long - really big whole number
+// short - just some whole number, smaller than int
+// byte - smallest whole number
+
+
