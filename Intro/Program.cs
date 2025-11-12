cb2c3c1cf64355d90dabd356abf477ff62ac0d46
Voxic <m.hille@voxicgames.com>
2025-11-12 12:22:03 +0100

    Added string array creation method and string array print method

--- Intro/Program.cs
+++ Intro/Program.cs
@@ -66,10 +66,10 @@ while (geraltsHealth > 0) // is being executed until geralt is dead -> hp < 0
     Console.WriteLine("Geralt attacks Monster");
     monsterHealth -= geraltsStrength;
     Console.WriteLine($"Monster is down to {monsterHealth} hp.");
-    
+
     if (monsterHealth <= 0) // if monster is already slain, break out of loop
         break; // terminates the loop
-    
+
     Console.WriteLine("Geralt is being attacked...");
     geraltsHealth -= monsterStrength;
     Console.WriteLine($"Geralt is down to {geraltsHealth} hp.");
@@ -103,7 +103,7 @@ for (int i = 0; i < weapons.Length; i++)
 Console.Write("Please enter the # of players joined: ");
 int playerCount = GetNumericValueFromString(Console.ReadLine()); // determine array size
 string[] playerNames = new string[playerCount]; // create array with set size
-for (int i = 0; i < playerCount; i++) 
+for (int i = 0; i < playerCount; i++)
 {
     Console.WriteLine("Please enter the name of the player: ");
     playerNames[i] = Console.ReadLine(); // store current player name
@@ -111,16 +111,24 @@ for (int i = 0; i < playerCount; i++)
 
 
 // Function call with playerNames array as argument
-PrintPlayerNames(playerNames);
+PrintTexts(playerNames);
+
+
+// Exercise array application -> Create, Fill, Read(Print)
+Console.WriteLine("Enter # of texts");
+var textFragmentCount = GetNumericValueFromString(Console.ReadLine());
+var texts = GetStringAarray(textFragmentCount);
+PrintTexts(texts);
+
 
 // functions 
 // returnType functionName(datatype parameterName) {..here goes the logic..}
 // Definition
-void PrintPlayerNames(string[] playerNames)
+void PrintTexts(string[] texts)
 {
-    foreach (var playerName in playerNames) 
+    foreach (var playerName in texts)
     {
-        Console.WriteLine(playerName);// print current player name observed
+        Console.WriteLine(playerName); // print current player name observed
     }
 }
 
@@ -130,3 +138,24 @@ int GetNumericValueFromString(string text)
 {
     return int.Parse(text);
 }
+
+// Exercise
+// create a method for generating a string array of a certain size 
+// Create a method to print all entries of this or any similar array
+
+string[] GetStringAarray(int size)
+{
+    string[] texts = new string[size]; // Create array of type string with # of elements 
+    FillStringArray(texts); // Call method to assign actual value to each slot
+    return texts; // deliver back filled array of given size
+}
+
+void FillStringArray(string[] stringArray)
+{
+    for (int i = 0; i < stringArray.Length; i++) // Loop through complete array
+    {
+        Console.WriteLine($"Enter value for {i} entry"); // Print what slot we are on
+        stringArray[i] = Console.ReadLine(); // Store actual value in current slot
+    }
+}
+
